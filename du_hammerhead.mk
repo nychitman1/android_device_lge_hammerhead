9b29f01a189c41cfd94aca370ed0be8a5675e6c4
Josh Chasky <jchasky@gmail.com>
2014-11-07 15:28:52 -0500

    Du-ify for Lollipop

---
+++ du_hammerhead.mk
@@ -0,0 +1,48 @@
+#
+# Copyright 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := vendor/du/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/du/config/common.mk)
+$(call inherit-product, vendor/du/config/gsm.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/lge/hammerhead/device.mk)
+
+PRODUCT_NAME := du_hammerhead
+PRODUCT_DEVICE := hammerhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:4.4.4/KTU84P/1227136:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 4.4.4 KTU84P 1227136 release-keys"
+
+# Kernel inline build
+#TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
+#TARGET_KERNEL_CONFIG := custom_hammerhead_defconfig
+#TARGET_VARIANT_CONFIG := custom_hammerhead_defconfig
+#TARGET_SELINUX_CONFIG := custom_hammerhead_defconfig
+
+$(call inherit-product-if-exists, vendor/lge/hammerhead/device-vendor.mk)
